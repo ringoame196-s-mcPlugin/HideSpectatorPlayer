4465a2e36cc5252d88185b735698ec647b7e38e6
りんご飴196 <kazumac41@gmail.com>
2025-05-01 16:31:41 +0900

    ⬆️ forをなくす

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/packetListener/PlayerInfoListener.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/packetListener/PlayerInfoListener.kt
@@ -1,11 +1,13 @@
 package com.github.ringoame196_s_mcPlugin.packetListener
 
 import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
 import com.comphenix.protocol.events.ListenerPriority
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
 import com.comphenix.protocol.wrappers.EnumWrappers
 import com.github.ringoame196_s_mcPlugin.Data
+import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 
 class PlayerInfoListener(plugin: Plugin) : PacketAdapter(plugin, ListenerPriority.NORMAL, PacketType.Play.Server.PLAYER_INFO) {
@@ -20,15 +22,13 @@ class PlayerInfoListener(plugin: Plugin) : PacketAdapter(plugin, ListenerPriorit
         if (packet.playerInfoDataLists.size() < 1) return
         val playerInfoData = packet.playerInfoDataLists.read(1)
 
-        // 複数プレイヤーに対応
-        for (info in playerInfoData) {
-            val gameMode = info.gameMode
-            val playerUUID = info.profile.id
+        val shouldCancel = playerInfoData.any { info ->
+            info.gameMode == EnumWrappers.NativeGameMode.SPECTATOR &&
+                    info.profile.id != sender.uniqueId.toString()
+        }
 
-            // スペク 本人以外には パケットを飛ばさない
-            if (gameMode == EnumWrappers.NativeGameMode.SPECTATOR && playerUUID != sender.uniqueId.toString()) {
-                e.isCancelled = true
-            }
+        if (shouldCancel) {
+            e.isCancelled = true
         }
     }
 }
